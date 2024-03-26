c1a2522ae9ef0841099d3f9686dde9ddc541f34a
Kevin Breid <kevinbreid@yahoo.com>
2024-03-26 18:30:55 +0100

    Configuring Script for Server

--- server/app.py
+++ server/app.py
@@ -5,6 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/'):
+@app.route('/')
 def index():
     return '<h1>You are on the app bb<h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'This is the home of {username}'
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
